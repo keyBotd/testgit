99fc70775c61a05a856c41db9ca6fe178d66c67f
Lomik8901 <fkuputdinov@mail.ru>
2024-06-21 19:44:46 +0300

    message

--- testing.py
+++ testing.py
@@ -1,53 +1,57 @@
-import unittest
-from nnnnn import triangle_type, delete_space
-from parameterized import parameterized
+# import unittest
+# from nnnnn import triangle_type, delete_space
+# from parameterized import parameterized
+#
+#
+# class TriangleTypeTest(unittest.TestCase):
+#     def test_ravnobedr(self):
+#         self.assertEqual('равнобедренный', triangle_type(9, 9, 8))
+#
+#     def test_isCorrectType(self):
+#         self.assertEqual('Неверный тип данных', triangle_type('vdjkf', 1, 3))
+#
+#     def test_something_triangle(self):
+#         self.assertEqual('произвольный', triangle_type(5, 6, 9))
+#
+#     def test_ravnostor(self):
+#         self.assertEqual('равносторонний', triangle_type(10, 10, 10))
+#
+#     def test_pryamoug_triangle(self):
+#         self.assertEqual('прямоугольный', triangle_type(5, 12, 13))
+#
+#     def test_storona(self):
+#         self.assertEqual('стороны должны быть больше нуля', triangle_type(0, 1, 2))
+#
+#     def test_sum_storon(self):
+#         self.assertEqual('это не треугольник', triangle_type(1, 2, 3))
+#
+#
+# class DeleteSpaceTest(unittest.TestCase):
+#     def test_normal_str(self):
+#         self.assertEqual('hello world', delete_space('hello world'))
+#
+#     def test_right_space(self):
+#         self.assertEqual('hello world', delete_space('hello world             '))
+#
+#     def test_left_space(self):
+#         self.assertEqual('hello world', delete_space('              hello world'))
+#
+#     def test_center_space(self):
+#         self.assertEqual('hello world', delete_space('hello        world'))
+#
+#     def test_empty_str(self):
+#         self.assertEqual('пустая строка', delete_space(''))
+#
+#     def test_space_str(self):
+#         self.assertEqual('пустая строка', delete_space('       '))
+#
+#     def test_not_space_str(self):
+#         self.assertEqual('helloworld', delete_space('helloworld'))
+#
+#
+# if __name__ == '__main__':
+#     unittest.main()
 
 
-class TriangleTypeTest(unittest.TestCase):
-    def test_ravnobedr(self):
-        self.assertEqual('равнобедренный', triangle_type(9, 9, 8))
 
-    def test_isCorrectType(self):
-        self.assertEqual('Неверный тип данных', triangle_type('vdjkf', 1, 3))
-
-    def test_something_triangle(self):
-        self.assertEqual('произвольный', triangle_type(5, 6, 9))
-
-    def test_ravnostor(self):
-        self.assertEqual('равносторонний', triangle_type(10, 10, 10))
-
-    def test_pryamoug_triangle(self):
-        self.assertEqual('прямоугольный', triangle_type(5, 12, 13))
-
-    def test_storona(self):
-        self.assertEqual('стороны должны быть больше нуля', triangle_type(0, 1, 2))
-
-    def test_sum_storon(self):
-        self.assertEqual('это не треугольник', triangle_type(1, 2, 3))
-
-
-class DeleteSpaceTest(unittest.TestCase):
-    def test_normal_str(self):
-        self.assertEqual('hello world', delete_space('hello world'))
-
-    def test_right_space(self):
-        self.assertEqual('hello world', delete_space('hello world             '))
-
-    def test_left_space(self):
-        self.assertEqual('hello world', delete_space('              hello world'))
-
-    def test_center_space(self):
-        self.assertEqual('hello world', delete_space('hello        world'))
-
-    def test_empty_str(self):
-        self.assertEqual('пустая строка', delete_space(''))
-
-    def test_space_str(self):
-        self.assertEqual('пустая строка', delete_space('       '))
-
-    def test_not_space_str(self):
-        self.assertEqual('helloworld', delete_space('helloworld'))
-
-
-if __name__ == '__main__':
-    unittest.main()
+dndnn
